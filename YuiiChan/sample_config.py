6e6f6a761eaf3af57f0a35f5399ae59e86ca881b
rientanakharam <123633789+rientanakharam@users.noreply.github.com>
2023-06-16 14:13:19 +0700

    Update sample_config.py

--- YuiiChan/sample_config.py
+++ YuiiChan/sample_config.py
@@ -14,18 +14,18 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "awoo"
-    API_HASH = "awoo"
-    API_KEY = "BOT_TOKEN"
-    OWNER_ID = "792109647"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "@OnePunchSupport"
+    API_ID = "27776767"
+    API_HASH = "e7b0d8f7b037df9ff8b300816e90080b"
+    API_KEY = "6038308153:AAHSpZ3aHzr0yQtVA4Uz5AoVUpuIPOrPr8M"
+    OWNER_ID = "6044333279"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "kazumikazo"
+    SUPPORT_CHAT = "@coldbloodnation"
 
     # RECOMMENDED
 
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
-    MESSAGE_DUMP = -1001253661229  # needed to make sure 'save from' messages persist
-    GBAN_LOGS = -1001190806654
+    SQLALCHEMY_DATABASE_URI = "postgres://cpdrwphc:LXuKxTftoPMnZ575JTP479F9Sf7f8XXg@tyke.db.elephantsql.com/cpdrwphc"  # needed for any database modules
+    MESSAGE_DUMP = -1001848461536  # needed to make sure 'save from' messages persist
+    GBAN_LOGS = -1001646756467
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
@@ -33,15 +33,15 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    SUDO_USERS = get_user_list("elevated_users.json", "sudos")
+    SUDO_USERS = 1952067206 5007562506 1178572412("elevated_users.json", "sudos")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEV_USERS = 1952067206 5007562506 1178572412("elevated_users.json", "devs")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    SUPPORT_USERS = get_user_list("elevated_users.json", "supports")
+    SUPPORT_USERS = 1952067206 5007562506 1178572412("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGER_USERS = get_user_list("elevated_users.json", "tigers")
-    WHITELIST_USERS = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = None  # EG, paypal
+    TIGER_USERS = 1952067206 5007562506 1178572412("elevated_users.json", "tigers")
+    WHITELIST_USERS = 1952067206 5007562506 1178572412("elevated_users.json", "whitelists")
+    DONATION_LINK = Https://t.me/kazumikazo  # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
